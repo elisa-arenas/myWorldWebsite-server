fd8797dbe8bbacb767e5ae5a6a2359a4de255555
Elisa Arenas <elisaarenas@ufl.edu>
2023-02-27 13:52:10 -0500

    init

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    MONGODB:
+        'mongodb+srv://user1:xFHCCltOA8icl0In@cluster0.iyjwf2t.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+};
+
+
+//password: xFHCCltOA8icl0In
+//connection string: mongodb+srv://user1:<password>@cluster0.iyjwf2t.mongodb.net/?retryWrites=true&w=majority
